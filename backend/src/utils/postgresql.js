de6f68a82c85226d3da892fe82c69be784b291c1
TheJuanJo1 <mbappejuan464@gmail.com>
2025-08-26 17:18:21 -0500

    Se añadio schema prisma, en posgresql Se coloco una tabla que era que es la de usuarios, se añadio el archivo .env

--- backend/src/utils/postgresql.js
+++ backend/src/utils/postgresql.js
@@ -2,7 +2,7 @@ import { pool } from "./database/conectionPosgreSQL.js";
 
 const getUsuarios = async () => {
   try {
-    const result = await pool.query("SELECT * FROM Usuarios;");
+    const result = await pool.query(`SELECT * FROM "Usuarios";`);
     console.log(result.rows);
   } catch (error) {
     console.error(error);
